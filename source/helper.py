2d61e9c4ea49aa5a7a0a4550a288bf7f9b90e232
Dario <ddematties@anl.gov>
2024-01-24 01:50:18 +0000

    Adding agent

--- source/helper.py
+++ source/helper.py
@@ -253,3 +253,48 @@ def init_opt(
         T_max=int(ipe_scale*num_epochs*iterations_per_epoch))
     scaler = torch.cuda.amp.GradScaler() if use_bfloat16 else None
     return optimizer, scaler, scheduler, wd_scheduler
+
+
+
+
+def init_agent_model(
+    device,
+    patch_size=16,
+    model_name='vit_base',
+    crop_size=224,
+    pred_depth=6,
+    pred_emb_dim=384,
+    num_actions=16
+):
+    encoder = vit.__dict__[model_name](
+        img_size=[crop_size],
+        patch_size=patch_size)
+    predictor = vit.__dict__['vit_agent'](
+        num_patches=encoder.patch_embed.num_patches,
+        embed_dim=encoder.embed_dim,
+        predictor_embed_dim=pred_emb_dim,
+        depth=pred_depth,
+        num_heads=encoder.num_heads,
+	num_actions=num_actions)
+
+    def init_weights(m):
+        if isinstance(m, torch.nn.Linear):
+            trunc_normal_(m.weight, std=0.02)
+            if m.bias is not None:
+                torch.nn.init.constant_(m.bias, 0)
+        elif isinstance(m, torch.nn.LayerNorm):
+            torch.nn.init.constant_(m.bias, 0)
+            torch.nn.init.constant_(m.weight, 1.0)
+
+    #for m in encoder.modules():
+    #    init_weights(m)
+
+    for m in predictor.modules():
+        init_weights(m)
+
+    encoder.to(device)
+    predictor.to(device)
+    logger.info(encoder)
+    return encoder, predictor
+
+
