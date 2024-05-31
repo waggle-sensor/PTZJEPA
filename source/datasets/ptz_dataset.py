753002d5b5947e48b736e9e469608528575f6998
Yufeng Luo <brookluo@yahoo.com>
2024-05-31 16:31:45 -0500

    Add notebook to test generating embeddings

--- source/datasets/ptz_dataset.py
+++ source/datasets/ptz_dataset.py
@@ -3,11 +3,18 @@ import pandas as pd
 from torchvision.io import read_image
 from PIL import Image 
 from torch.utils.data import Dataset
+from pathlib import Path
 
 class PTZImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file, header=None)
-        self.img_dir = img_dir
+    def __init__(self, img_dir, annotations_file=None, transform=None, target_transform=None):
+        if not os.path.isdir(img_dir):
+            raise FileNotFoundError(f'{img_dir} is not a directory')
+        self.img_dir = Path(img_dir)
+        if annotations_file is None:
+            self.img_labels = [im.stem for im in self.img_dir.glob('*.jpg')]
+        else:
+            # the filename has only one column
+            self.img_labels = list(pd.read_csv(annotations_file, header=None)[0])
         self.transform = transform
         self.target_transform = target_transform
 
@@ -16,11 +23,11 @@ class PTZImageDataset(Dataset):
 
     def __getitem__(self, idx):
         #img_path = os.path.join(self.img_dir, self.img_labels[idx,0] + '.jpg')
-        img_path = os.path.join(self.img_dir, self.img_labels[0][idx] + '.jpg')
+        img_path = self.img_dir / (self.img_labels[idx] + '.jpg')
         image = Image.open(img_path)
         #image = read_image(img_path)
         #label = self.img_labels[idx,0]
-        label = self.img_labels[0][idx]
+        label = self.img_labels[idx]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
