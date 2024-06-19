Fix no gradient error on multi-pass

source/run_jepa.py
@@ -723,10 +723,9 @@ def world_model(args, logger=None, resume_preempt=False):
         # Step 1. Auxiliary Forward
         h = forward_target(inputs[2], target_encoder)
         # ! for pytorch<2, Needs all gradients for backpropagation 
-        with torch.no_grad():
-            z, r = forward_context(inputs[0], inputs[1], inputs[3],
-                                   encoder, predictor, camera_brand)
-        z = z.detach()
+        # with torch.no_grad():
+        z, r = forward_context(inputs[0], inputs[1], inputs[3],
+                                encoder, predictor, camera_brand)
         auxiliary_loss = auxiliary_loss_fn(z, h)
 
         # Step 2. Auxiliary Backward
@@ -738,15 +737,16 @@ def world_model(args, logger=None, resume_preempt=False):
         grads = torch.cat(grads)
         g = grads.abs().sum()
         target_encoder.zero_grad()
-        # do not update the gradient for context branch yet
-        # predictor.zero_grad()
-        # encoder.zero_grad()
+        # do not update the gradient for context branch for the auxiliary
+        # gradient calculation pass
+        predictor.zero_grad()
+        encoder.zero_grad()
+        # ? Is it equivalent to optimizer.zero_grad()?
 
         # Step 3. Forward
-        with torch.no_grad():
-            # EMA update for target encoder
-            h = forward_target(inputs[2], target_encoder)
-        h = h.detach()
+        # with torch.no_grad():
+        # EMA update for target encoder
+        h = forward_target(inputs[2], target_encoder)
         # Need to update the gradient
         z, r = forward_context(inputs[0], inputs[1], inputs[3],
                                encoder, predictor, camera_brand)
@@ -756,7 +756,8 @@ def world_model(args, logger=None, resume_preempt=False):
         loss.backward()
         # do not update the gradient for target encoder
         # update is done via EMA
-        # target_encoder.zero_grad()
+        target_encoder.zero_grad()
+
         optimizer.step()
         grad_stats = grad_logger(encoder.named_parameters())
         optimizer.zero_grad()