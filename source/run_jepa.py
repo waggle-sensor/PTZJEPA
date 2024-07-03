Incorporate additional actions. Fixing reward computation. Stochasticity in camera behavior.

source/run_jepa.py
@@ -726,7 +726,8 @@ def world_model(args, logger=None, resume_preempt=False):
             if param.grad is not None:
                 grads.append(param.grad.view(-1))
         grads = torch.cat(grads)
-        g = grads.abs().sum()
+        #g = grads.abs().sum()
+        g = grads.abs().mean()
         target_encoder.zero_grad()
         # do not update the gradient for context branch for the auxiliary
         # gradient calculation pass
@@ -782,7 +783,8 @@ def world_model(args, logger=None, resume_preempt=False):
             if param.grad != None:
                 grads.append(param.grad.view(-1))
         grads = torch.cat(grads)
-        g = grads.abs().sum()
+        #g = grads.abs().sum()
+        g = grads.abs().mean()
         target_encoder.zero_grad()
 
         # Step 3. Forward
@@ -817,6 +819,9 @@ def world_model(args, logger=None, resume_preempt=False):
     def loss_fn(z, r, h, g):
         loss1 = F.smooth_l1_loss(z, h)
         loss2 = F.smooth_l1_loss(r, g.repeat(r.shape))# * 1e-4
+        #print('reward shape ', r.shape)
+        #print('reward mean ', r.mean())
+        #print('reward ', g)
         loss = loss1 + loss2
         return loss
 
@@ -967,6 +972,7 @@ def dreamer(args, logger=None, resume_preempt=False):
     tag = args['logging']['write_tag']
 
     # -- ACTIONS
+    action_noop = args['action']['noop']
     action_short_left = args['action']['short']['left']
     action_short_right = args['action']['short']['right']
     action_short_left_up = args['action']['short']['left_up']
@@ -985,23 +991,33 @@ def dreamer(args, logger=None, resume_preempt=False):
     action_long_zoom_in = args['action']['long']['zoom_in']
     action_long_zoom_out = args['action']['long']['zoom_out']
 
+    action_jump_left = args['action']['jump']['left']
+    action_jump_right = args['action']['jump']['right']
+    action_jump_up = args['action']['jump']['up']
+    action_jump_down = args['action']['jump']['down']
+
     actions={}
-    actions[0]=action_short_left
-    actions[1]=action_short_right
-    actions[2]=action_short_left_up
-    actions[3]=action_short_right_up
-    actions[4]=action_short_left_down
-    actions[5]=action_short_right_down
-    actions[6]=action_short_up
-    actions[7]=action_short_down
-    actions[8]=action_short_zoom_in
-    actions[9]=action_short_zoom_out
-    actions[10]=action_long_left
-    actions[11]=action_long_right
-    actions[12]=action_long_up
-    actions[13]=action_long_down
-    actions[14]=action_long_zoom_in
-    actions[15]=action_long_zoom_out
+    actions[0]=action_noop
+    actions[1]=action_short_left
+    actions[2]=action_short_right
+    actions[3]=action_short_left_up
+    actions[4]=action_short_right_up
+    actions[5]=action_short_left_down
+    actions[6]=action_short_right_down
+    actions[7]=action_short_up
+    actions[8]=action_short_down
+    actions[9]=action_short_zoom_in
+    actions[10]=action_short_zoom_out
+    actions[11]=action_long_left
+    actions[12]=action_long_right
+    actions[13]=action_long_up
+    actions[14]=action_long_down
+    actions[15]=action_long_zoom_in
+    actions[16]=action_long_zoom_out
+    actions[17]=action_jump_left
+    actions[18]=action_jump_right
+    actions[19]=action_jump_up
+    actions[20]=action_jump_down
 
     # -- MEMORY
     memory_dreams = args['memory']['dreams']